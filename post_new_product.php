9f98d4235aa5101826aa7a3880269f397f90674a
Zulqarnain <zuluparvi@gmail.com>
2018-10-04 15:51:54 +0500

    Update post_new_product.php

--- post_new_product.php
+++ post_new_product.php
@@ -25,3 +25,33 @@ if ($run) {
 }
 print_r($data)
 ?>
+
+<?php
+$data = json_decode(file_get_contents('php://input'), true);;
+$photo = $data['image'];
+$price = $data['Saving'];
+//echo "test ".$price;
+
+$name = rand(). ".jpeg";
+
+define('UPLOAD_DIR',  'uploads/');
+    $image_parts = explode(";base64,", $photo);
+     // $image_type_aux = explode("image/", $image_parts[0]);
+	$image_base64 = base64_decode($image_parts[0]);
+
+	$name = rand(). ".jpeg";
+
+if(file_put_contents(UPLOAD_DIR .$name, $image_base64)){
+  	echo json_encode(["Message" => $name,
+						"Status" => "OK"]);
+    
+}else{
+	echo json_encode(["Message" => "Sorry, Not Successful",
+						"Status" => $photo]);	
+						
+}
+
+
+
+
+?>
